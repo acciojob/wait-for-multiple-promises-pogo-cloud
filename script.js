5a359b8175e4f4a20a91d0f30e25f9e022782637
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-11-12 16:09:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -39,6 +39,16 @@ let table=document.querySelector(".table");
           tbody.append(trow);
           table.appendChild(tbody);
         }
-      }).catch((error)=>{
-          console.log(error);
-      })
+        	  let trow=document.createElement('tr');
+            let cell1=document.createElement('td');
+            let cell2=document.createElement('td');
+            cell1.textContent="Total";
+            cell2.textContent=Math.max(...result) / 1000;
+            trow.appendChild(cell1);
+            trow.appendChild(cell2);
+            tbody.append(trow);
+            table.appendChild(tbody);
+        }).catch((error)=>{
+            console.log(error);
+        })
+    
